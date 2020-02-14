8407de6f1e6240428a72a21f6ee365580dbe9eee
Chris Kypridemos <ckyprid@liverpool.ac.uk>
2020-02-14 14:20:04 +0000

    add server-side files

---
+++ server/save_load_scenarios.R
@@ -0,0 +1,100 @@
+## workHORSE is an implementation of the IMPACTncd framework, developed by Chris
+## Kypridemos with contributions from Peter Crowther (Melandra Ltd), Maria
+## Guzman-Castillo, Amandine Robert, and Piotr Bandosz. This work has been
+## funded by NIHR  HTA Project: 16/165/01 - workHORSE: Health Outcomes
+## Research Simulation Environment.  The views expressed are those of the
+## authors and not necessarily those of the NHS, the NIHR or the Department of
+## Health.
+##
+## Copyright (C) 2018-2020 University of Liverpool, Chris Kypridemos
+##
+## workHORSE is free software; you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free Software
+## Foundation; either version 3 of the License, or (at your option) any later
+## version. This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+## FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+## details. You should have received a copy of the GNU General Public License
+## along with this program; if not, see <http://www.gnu.org/licenses/> or write
+## to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+## Boston, MA 02110-1301 USA.
+
+# From https://stackoverflow.com/questions/32922190/saving-state-of-shiny-app-to-be-restored-later
+output$save_sc1 <- downloadHandler(
+
+
+  filename = function()
+    paste0(input$friendly_name_sc1, ".rds"),
+
+  content = function(file) {
+    updateCollapse(session, "collapse_panel_sc1",
+    open = c("Eligibility Criteria",
+             "Appointments Offered Yearly (%)",
+             "Health Checks Received (%)",
+             "Prescription Rate",
+             "Impact on Lifestyle",
+             "Advanced",
+             "Notes"))
+    Sys.sleep(2)
+   tt_sc1 <- lapply(reactiveValuesToList(input), unclass)
+
+    # tt_sc1 <- reactiveValuesToList(input)
+    tt_sc1 <- tt_sc1[grep("^.*_sc1$", names(tt_sc1))]
+    tt_sc1$save_sc1 <- NULL
+      tt_sc1$load_sc1 <- NULL
+      tt_sc1$collapse_panels_button_sc1 <- NULL
+      tt_sc1$collapse_panel_sc1 <- NULL
+      tt_sc1$run_simulation_sc1 <- NULL
+
+    tt_sc1 <- setNames(tt_sc1, gsub("_sc1$", "", names(tt_sc1)))
+    saveRDS(tt_sc1, file = file)
+  }
+)
+
+
+  observeEvent(input$load_sc1, {
+    updateCollapse(session, "collapse_panel_sc1",
+                   open = c("Eligibility Criteria",
+                            "Appointments Offered Yearly (%)",
+                            "Health Checks Received (%)",
+                            "Prescription Rate",
+                            "Impact on Lifestyle",
+                            "Advanced",
+                            "Notes"))
+
+    inFile <- input$load_sc1
+
+    if (is.null(inFile)) return(NULL)
+    savedInputs   <- readRDS(inFile$datapath)
+    names(savedInputs)      <- paste0(names(savedInputs), "_sc1")
+
+    if (savedInputs[["uptake_detailed_checkbox_sc1"]]) showElement(id = "uptake_table_sc1")
+ if (savedInputs[["px_detailed_checkbox_sc1"]]) {
+   showElement(id = "statin_px_table_sc1")
+   showElement(id = "antihtn_px_table_sc1")
+ }
+
+# delay necessary so all elements are expanded on screen before load
+    delay(2000, lapply(names(savedInputs),
+           function(x) session$sendInputMessage(x, list(value = savedInputs[[x]]))
+    ))
+  })
+
+# Ensure uptake & px is open and close once, otherwise the tables are not produced and they cannot be saved or load
+observeEvent(input$collapse_panels_button_sc1, ({
+  if (length(input$collapse_panel_sc1) > 1) {
+    updateCollapse(session, "collapse_panel_sc1",
+                   close = setdiff(input$collapse_panel_sc1, "General Parameters"))
+  } else {
+    updateCollapse(session, "collapse_panel_sc1",
+                   open = c("General Parameters",
+                            "Eligibility Criteria",
+                            "Appointments Offered Yearly (%)",
+                            "Health Checks Received (%)",
+                            "Prescription Rate",
+                            "Impact on Lifestyle"#,
+                           # "Advanced",
+                           # "Notes"
+                            ))
+  }
+}))
---
+++ server/show_hide_logic.R
@@ -0,0 +1,216 @@
+## workHORSE is an implementation of the IMPACTncd framework, developed by Chris
+## Kypridemos with contributions from Peter Crowther (Melandra Ltd), Maria
+## Guzman-Castillo, Amandine Robert, and Piotr Bandosz. This work has been
+## funded by NIHR  HTA Project: 16/165/01 - workHORSE: Health Outcomes
+## Research Simulation Environment.  The views expressed are those of the
+## authors and not necessarily those of the NHS, the NIHR or the Department of
+## Health.
+##
+## Copyright (C) 2018-2020 University of Liverpool, Chris Kypridemos
+##
+## workHORSE is free software; you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free Software
+## Foundation; either version 3 of the License, or (at your option) any later
+## version. This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+## FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+## details. You should have received a copy of the GNU General Public License
+## along with this program; if not, see <http://www.gnu.org/licenses/> or write
+## to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+## Boston, MA 02110-1301 USA.
+
+observeEvent(input$cancel_program_checkbox_sc1, {
+  if (input$cancel_program_checkbox_sc1) {
+    lapply(
+      c(
+        "frequency_eligibility_slider_sc1",
+        "age_eligibility_slider_sc1",
+        "invite_known_hypertensives_checkbox_sc1",
+        "invite_known_diabetics_checkbox_sc1",
+        "coverage_detailed_checkbox_sc1",
+        "coverage_qimd0_slider_sc1",
+        "coverage_qimd1_slider_sc1",
+        "coverage_qimd2_slider_sc1",
+        "coverage_qimd3_slider_sc1",
+        "coverage_qimd4_slider_sc1",
+        "coverage_qimd5_slider_sc1",
+        "coverage_cost_qimd0_sc1",
+        "coverage_cost_qimd1_sc1",
+        "coverage_cost_qimd2_sc1",
+        "coverage_cost_qimd3_sc1",
+        "coverage_cost_qimd4_sc1",
+        "coverage_cost_qimd5_sc1",
+        "uptake_slider_sc1",
+        "uptake_cost_sc1",
+        "uptake_detailed_checkbox_sc1",
+        "uptake_equalprob_checkbox_sc1",
+        "uptake_table_help_sc1",
+        "uptake_table_sc1",
+        "statin_px_slider_sc1",
+        "antihtn_px_slider_sc1",
+        "px_detailed_checkbox_sc1",
+        "statin_px_table_help_sc1",
+        "statin_px_table_sc1",
+        "antihtn_px_table_help_sc1",
+        "antihtn_px_table_sc1",
+        "smkcess_slider_sc1",
+        "smkcess_cost_sc1",
+        "smkcess_cost_ovrhd_sc1",
+        "wghtpct_slider_sc1",
+        "wghtreduc_slider_sc1",
+        "wghtloss_cost_sc1",
+        "wghtloss_cost_ovrhd_sc1",
+        "papct_slider_sc1",
+        "papincr_slider_sc1",
+        "pa_cost_sc1",
+        "pa_cost_ovrhd_sc1",
+        "ignore_cholesterol_checkbox_sc1",
+        "ignore_sbp_checkbox_sc1",
+        "ignore_bmi_checkbox_sc1"
+      ),
+      hideElement,
+      anim = TRUE
+    )
+  } else {
+    lapply(
+      c(
+        "frequency_eligibility_slider_sc1",
+        "age_eligibility_slider_sc1",
+        "invite_known_hypertensives_checkbox_sc1",
+        "invite_known_diabetics_checkbox_sc1",
+        "coverage_detailed_checkbox_sc1",
+        "smkcess_slider_sc1",
+        "smkcess_cost_sc1",
+        "smkcess_cost_ovrhd_sc1",
+        "wghtpct_slider_sc1",
+        "wghtreduc_slider_sc1",
+        "wghtloss_cost_sc1",
+        "wghtloss_cost_ovrhd_sc1",
+        "papct_slider_sc1",
+        "papincr_slider_sc1",
+        "pa_cost_sc1",
+        "pa_cost_ovrhd_sc1",
+        "uptake_slider_sc1",
+        "uptake_cost_sc1",
+        "uptake_detailed_checkbox_sc1",
+        "px_detailed_checkbox_sc1",
+        "ignore_cholesterol_checkbox_sc1",
+        "ignore_sbp_checkbox_sc1",
+        "ignore_bmi_checkbox_sc1"
+      ),
+      showElement,
+      anim = TRUE
+    )
+    if (!input$coverage_detailed_checkbox_sc1) {
+      lapply(
+        c(
+          "coverage_qimd0_slider_sc1",
+          "coverage_cost_qimd0_sc1"
+        ),
+        showElement,
+        anim = TRUE
+      )
+    } else {
+      lapply(
+        c(
+          "coverage_qimd1_slider_sc1",
+          "coverage_qimd2_slider_sc1",
+          "coverage_qimd3_slider_sc1",
+          "coverage_qimd4_slider_sc1",
+          "coverage_qimd5_slider_sc1",
+          "coverage_cost_qimd1_sc1",
+          "coverage_cost_qimd2_sc1",
+          "coverage_cost_qimd3_sc1",
+          "coverage_cost_qimd4_sc1",
+          "coverage_cost_qimd5_sc1"
+        ),
+        showElement,
+        anim = TRUE
+      )
+    }
+    if (input$uptake_detailed_checkbox_sc1) {
+      lapply(
+        c(
+          "uptake_table_sc1",
+          "uptake_table_help_sc1",
+          "uptake_structural_zeroes_sc1"
+        ),
+        showElement,
+        anim = TRUE
+      )
+    } else {
+      # showElement(id = "uptake_equalprob_checkbox_sc1",
+      #             anim = TRUE)
+    }
+
+    if (!input$px_detailed_checkbox_sc1) {
+      lapply(c("statin_px_slider_sc1", "antihtn_px_slider_sc1"),
+             showElement,
+             anim = TRUE)
+    } else {
+      lapply(
+        c(
+          "statin_px_slider_sc1",
+          "antihtn_px_slider_sc1",
+          "statin_px_table_help_sc1",
+          "statin_px_table_sc1",
+          "antihtn_px_table_help_sc1",
+          "antihtn_px_table_sc1"
+        ),
+        showElement,
+        anim = TRUE
+      )
+    }
+  }
+})
+
+observeEvent(input$coverage_detailed_checkbox_sc1, {
+  toggleElement(id = "coverage_qimd0_slider_sc1",
+                condition = !input$coverage_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "coverage_cost_qimd0_sc1",
+                condition = !input$coverage_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "coverage_qimd1_slider_sc1",
+                condition = input$coverage_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "coverage_cost_qimd1_sc1",
+                condition = input$coverage_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "coverage_qimd2_slider_sc1",
+                condition = input$coverage_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "coverage_cost_qimd2_sc1",
+                condition = input$coverage_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "coverage_qimd3_slider_sc1",
+                condition = input$coverage_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "coverage_cost_qimd3_sc1",
+                condition = input$coverage_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "coverage_qimd4_slider_sc1",
+                condition = input$coverage_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "coverage_cost_qimd4_sc1",
+                condition = input$coverage_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "coverage_qimd5_slider_sc1",
+                condition = input$coverage_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "coverage_cost_qimd5_sc1",
+                condition = input$coverage_detailed_checkbox_sc1, anim = TRUE)
+})
+
+observeEvent(input$uptake_detailed_checkbox_sc1, {
+  toggleElement(id = "uptake_table_sc1",
+                condition = input$uptake_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "uptake_table_help_sc1",
+                condition = input$uptake_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "uptake_structural_zeroes_sc1",
+                condition = input$uptake_detailed_checkbox_sc1, anim = TRUE)
+})
+
+observeEvent(input$px_detailed_checkbox_sc1, {
+  # toggleElement(id = "statin_px_slider_sc1",
+  #               condition = !input$px_detailed_checkbox_sc1, anim = TRUE)
+  # toggleElement(id = "antihtn_px_slider_sc1",
+  #               condition = !input$px_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "statin_px_table_help_sc1",
+                condition = input$px_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "statin_px_table_sc1",
+                condition = input$px_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "antihtn_px_table_help_sc1",
+                condition = input$px_detailed_checkbox_sc1, anim = TRUE)
+  toggleElement(id = "antihtn_px_table_sc1",
+                condition = input$px_detailed_checkbox_sc1, anim = TRUE)
+})
