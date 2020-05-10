cfc675890948326848d885fea02f20b1070c3319
Chris Kypridemos <ckyprid@liverpool.ac.uk>
2020-05-10 17:58:59 +0100

    Improvements in R6 classes Design & SynthPop

--- Rpackage/workHORSE_model_pkg/R/Design_class.R
+++ Rpackage/workHORSE_model_pkg/R/Design_class.R
@@ -22,7 +22,12 @@
 
 #' R6 Class representing a simulation design
 #'
+#' @description
 #' A design has a sim_prm list that holds the simulation parameters.
+#'
+#' @details
+#' To be completed...
+#'
 #' @export
 Design <-
   R6::R6Class(classname = "Design",
@@ -31,21 +36,22 @@ Design <-
             #' @field sim_prm The simulation parameters.
             sim_prm = NA,
 
-            #' @description
-            #' Create a new design object.
-            #' @param l Either a path to a yaml file or a list with appropriate format.
+            #' @description Create a new design object.
+            #' @param sim_prm Either a path to a yaml file or a list with appropriate
+            #'   format.
             #' @return A new `Design` object.
             #' @examples
             #' design <- Design$new("./validation/design_for_trends_validation.yaml")
-            initialize = function(l) {
+            initialize = function(sim_prm) {
 
-              data_type <- typeof(l)
+              data_type <- typeof(sim_prm)
               if (data_type == "character") {
-                l <- read_yaml(base::normalizePath(l, mustWork = TRUE))
+                sim_prm <- read_yaml(base::normalizePath(sim_prm, mustWork = TRUE))
 
               } else if (data_type != "list") {
 
-                stop("You can initialise the object only with an R object of type `list` or a path to a YAML configuration file")
+                stop("You can initialise the object only with an R object of
+                     type `list` or a path to a YAML configuration file")
               }
 
               # Validation
@@ -61,7 +67,6 @@ Design <-
                   "strata_for_output"     ,
                   "exposures"             ,
                   "n"                     ,
-                  "init_year"             ,
                   "init_year_long"        ,
                   "sim_horizon_max"       ,
                   "ageL"                  ,
@@ -80,29 +85,65 @@ Design <-
                   "output_dir"            ,
                   "cancer_cure"           ,
                   "validation"            ,
-                  "max_prvl_for_outputs"  ,
-                  "cvd_lag_enum"          ,
-                  "copd_lag_enum"         ,
-                  "cancer_lag_enum"       ,
-                  "nonmodelled_lag_enum"  ,
-                  "locality"              ,
-                  "init_year_fromGUI"     ,
-                  "sim_horizon_fromGUI"
-                ) %in% names(l)
+                  "max_prvl_for_outputs"
+                ) %in% names(sim_prm),
+
+                any(sim_prm$clusternumber == 1L, sim_prm$n_cpus == 1L),
+                sapply(sim_prm, function(x) if (is.numeric(x)) x >= 1 else TRUE)
               )
 
-              self$sim_prm = l
+              tt <- sim_prm[grepl("_lag$", names(sim_prm))]
+              l <- lapply(names(tt), function(x) {
+                disease_enum <- (cumsum(unlist(tt) == tt[[x]]) * duplicated(tt))[[x]]
+                if (disease_enum == 0L) disease_enum <- 1L
+                names(disease_enum) <- NULL
+                return(disease_enum)
+              })
+              names(l) <- paste0(names(tt), "_enum")
+              sim_prm <- c(sim_prm, l)
+
+              sim_prm$init_year <- sim_prm$init_year_long - 2000L
+
+              # place holders to be updated from self$update_fromGUI(parameters)
+
+              sim_prm$national_qimd       <- TRUE
+              sim_prm$init_year_fromGUI   <- sim_prm$init_year
+              sim_prm$sim_horizon_fromGUI <- sim_prm$sim_horizon_max
+              sim_prm$locality            <- "England"
+
+              # Create synthpop_dir_ if it doesn't exists
+              sim_prm$output_dir <-
+                base::normalizePath(sim_prm$output_dir, mustWork = FALSE)
+              if (!dir.exists(sim_prm$output_dir)) {
+                dir.create(sim_prm$output_dir, recursive = TRUE)
+                message(paste0("Directory ", sim_prm$output_dir, " was created"))
+              }
+
+
+              self$sim_prm = sim_prm
 
               invisible(self)
             },
 
-            #' @description
-            #' Create a new design object.
-            #' @param path Path including filename and extension to save a yaml file with the simulation parameters.
+            #' @description Create a new design object.
+            #' @param path Path including filename and extension to save a yaml
+            #'   file with the simulation parameters.
             #' @return The `Design` object.
             save_to_disk = function(path) {
               write_yaml(self$sim_prm, base::normalizePath(path, mustWork = FALSE))
 
+              invisible(self)
+            },
+
+            #' @description Updates the design object from GUI.
+            #' @param GUI_prm A GUI parameter object.
+            #' @return The `Design` object.
+            update_fromGUI = function(GUI_prm = parameters) {
+              self$sim_prm$national_qimd       <- GUI_prm$national_qimd_checkbox
+              # T = use national qimd, F = use local qimd
+              self$sim_prm$init_year_fromGUI   <- fromGUI_timeframe(GUI_prm)["init year"] - 2000L
+              self$sim_prm$sim_horizon_fromGUI <- fromGUI_timeframe(GUI_prm)["horizon"]
+              self$sim_prm$locality            <- fromGUI_location(GUI_prm)
               invisible(self)
             },
 
