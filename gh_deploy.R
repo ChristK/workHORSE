193f778b5a5cfd6e9270cdad5a2acf3a075514d8
Chris Kypridemos <ckyprid@liverpool.ac.uk>
2021-05-17 15:16:37 +0100

    fix deploy logic

--- gh_deploy.R
+++ gh_deploy.R
@@ -1,6 +1,16 @@
 library(piggyback)
 tag <- "v0.0.2"
 repo <- "ChristK/workHORSE"
-# pb_new_release(repo, tag)
 
-pb_download(repo = repo, tag = tag, show_progress = FALSE)
+if (interactive()) {
+  print("interactive")
+  pb_download(repo = repo, tag = tag, show_progress = TRUE)
+} else { # used with Rscript
+  # i.e. Rscript /root/workHORSE/gh_deploy.R "/root/workHORSE/"
+  args <- commandArgs(TRUE)
+  print(args)
+  pb_download(dest = args, repo = repo, tag = tag, show_progress = FALSE)
+}
+
+
+#
