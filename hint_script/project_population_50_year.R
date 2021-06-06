7bc87e5e730867a3de0fc26ddaccf32e37c00763
Vincy Huang <vincyh@dh012142.liv.ac.uk>
2021-06-06 17:04:30 +0100

    script for population projection

--- hint_script/project_population_50_year.R
+++ hint_script/project_population_50_year.R
@@ -20,8 +20,18 @@ population_train <- copy(population_before_2041)
 population_train[, sex := factor(sex, levels = c("men", "women"), labels = c("men", "women"))]
 
 model_population<- glm(pops ~ age + sex + year + LAD17CD , family = gaussian, data = population_train)
-qsave(model_population, "./preparatory_work/marginal_distr_smok_quit_yrs.qs")
+qsave(model_population, "./hint_script/population_prediction.qs")
 
 # pedict till 2071 ####
+model <- qread("./hint_script/population_prediction.qs")
 population_predict <- CJ(age = unique(population_train$age), sex = unique(population_train$sex), 
               year = 2042: 2071)
+test <- copy(population_predict)
+test <- test[, LAD17CD := "E06000001"]
+test <- test[, pops := predict.glm(model, newdata = test)]
+test <- test[, .(LAD17CD, sex, age, year, pops)]
+test_all <- rbind(population_train[, c(1, 3:6)], test)
+data_plot <- test_all[pop_sum := summary()]
+data_plot <- test_all[LAD17CD == "E06000001"][, .(pop_sum = sum(pops)), by = .(year, sex)]
+ggplot() + 
+  geom_point(data = data_plot, aes(x = year, y = pop_sum, group = sex))
